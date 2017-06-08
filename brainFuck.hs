b2d7ef824285d8c3aaf3356ce01234421d24a8f5
Agnishom Chattopadhyay <cagnishom@gmail.com>
2017-06-08 10:43:17 +0530

    Support for the `,` instruction.

--- brainFuck.hs
+++ brainFuck.hs
@@ -11,6 +11,7 @@ data Instruction = Lft
             | Nop
             | Loop Program
             | Out
+            | Inp
             deriving (Show, Eq)
 
 data Memory = Memory [Int] [Int]
@@ -34,28 +35,38 @@ parse xs = go xs []
       go ('+':xs) is   = go xs (Right Inc:is)
       go ('-':xs) is   = go xs (Right Dec:is)
       go ('.':xs) is   = go xs (Right Out:is)
+      go (',':xs) is   = go xs (Right Inp:is)
 
-runInstruction :: Memory -> Instruction -> (Memory, IO ())
-runInstruction mem Nop = (mem, return ())
-runInstruction mem@(Memory _ (x:xs)) Out = (mem, putChar (chr x))
-runInstruction (Memory xs (y:[])) Rgt = ((Memory (y:xs) [0]), return ())
-runInstruction (Memory xs (y:ys)) Rgt = ((Memory (y:xs) ys), return ())
-runInstruction (Memory (x:xs) ys) Lft = ((Memory xs (x:ys)), return ())
-runInstruction (Memory [] ys) Lft = ((Memory [] (0:ys)), return ())
-runInstruction (Memory xs (y:ys)) Inc = ((Memory xs ((y+1):ys)), return ())
-runInstruction (Memory xs (y:ys)) Dec = ((Memory xs ((y-1):ys)), return ())
-runInstruction mem@(Memory xs (0:ys)) (Loop _) = (mem, return ())
-runInstruction mem (Loop prog) = let (mem', act') = runProgram mem prog in 
-                              let (mem'', act'') = runInstruction mem' (Loop prog) in
-                                 (mem'', act' >> act'')
+runInstruction :: Memory -> Instruction -> IO (IO Memory, IO ())
+runInstruction mem Nop = return (return mem, return ())
+runInstruction (Memory xs (y:[])) Rgt = return (return (Memory (y:xs) [0]), return ())
+runInstruction (Memory xs (y:ys)) Rgt = return (return (Memory (y:xs) ys), return ())
+runInstruction (Memory (x:xs) ys) Lft = return (return (Memory xs (x:ys)), return ())
+runInstruction (Memory [] ys) Lft = return (return (Memory [] (0:ys)), return ())
+runInstruction (Memory xs (y:ys)) Inc = return (return (Memory xs ((y+1):ys)), return ())
+runInstruction (Memory xs (y:ys)) Dec = return (return (Memory xs ((y-1):ys)), return ())
+runInstruction mem@(Memory _ (x:xs)) Out = return (return mem, putChar (chr x))
+runInstruction (Memory xs (y:ys)) Inp = return (mem', return ())
+                                          where mem' = getChar >>= \z -> return (Memory xs ((ord z):ys))
+runInstruction mem@(Memory xs (0:ys)) (Loop _) = return (return mem, return ())
+runInstruction mem (Loop prog) = do
+                                    (mem1, act') <- runProgram mem prog
+                                    mem' <- mem1
+                                    (mem'', act'') <- runInstruction mem' (Loop prog)
+                                    return (mem'', act' >> act'')
 
-runProgram :: Memory -> Program -> (Memory, IO ())
-runProgram mem [] = (mem, return ())
-runProgram mem (i:is) = let (mem', act') = runInstruction mem i in
-                     let (mem'', act'') = runProgram mem' is in
-                        (mem'', act' >> act'')
+
+
+runProgram :: Memory -> Program -> IO (IO Memory, IO ())
+runProgram mem [] = return (return mem, return ())
+runProgram mem (i:is) = do
+                           (mem1, act1) <- runInstruction mem i
+                           mem' <- mem1
+                           (mem2, act2) <- runProgram mem' is
+                           return (mem2, act1 >> act2)
 
 main :: IO ()
 main = do
    prog <- getContents
-   snd $ runProgram emptyMem (parse (filter (`elem` "[]+-><.") prog))
+   (mem, act) <- runProgram emptyMem (parse (filter (`elem` "[]+-><.,") prog))
+   act
