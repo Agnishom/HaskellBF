c3c7de723cc99ce48a8816cc2c3ac146ff41ef00
Agnishom Chattopadhyay <cagnishom@gmail.com>
2021-11-18 17:46:57 -0600

    after 5 years

--- README.md
+++ README.md
@@ -6,17 +6,16 @@ A BrainFuck Interpreter in Haskell
 1. `git clone https://github.com/Agnishom/HaskellBF.git`
 2. `cd haskellBF`
 3. `ghc brainFuck.hs`
-4. `cat program.bf | ./brainFuck`
+4. `./brainFuck hello.bf`
 
 ## Example
 
 ```bash
-[agnishom@agnishom-pc haskellCode]$ ./brainFuck 
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.Hello World!
-[agnishom@agnishom-pc haskellCode]$ ghc brainFuck.hs 
+[agnishom@agnishom-pc HaskellBF]$ cat hello.bf 
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
+[agnishom@agnishom-pc HaskellBF]$ ghc brainFuck.hs 
 [1 of 1] Compiling Main             ( brainFuck.hs, brainFuck.o )
 Linking brainFuck ...
-[agnishom@agnishom-pc haskellCode]$ ./brainFuck 
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
+[agnishom@agnishom-pc HaskellBF]$ ./brainFuck hello.bf
 Hello World!
 ```
--- brainFuck.hs
+++ brainFuck.hs
@@ -3,10 +3,12 @@ import Data.List
 import Data.Char
 import System.Environment
 import System.IO
+import Control.Monad
 
 type Program = [Instruction]
 
-data Instruction = Lft
+data Instruction = 
+              Lft
             | Rgt
             | Inc
             | Dec
@@ -39,38 +41,30 @@ parse xs = go xs []
       go ('.':xs) is   = go xs (Right Out:is)
       go (',':xs) is   = go xs (Right Inp:is)
 
-runInstruction :: Memory -> Instruction -> IO (IO Memory, IO ())
-runInstruction mem Nop = return (return mem, return ())
-runInstruction (Memory xs (y:[])) Rgt = return (return (Memory (y:xs) [0]), return ())
-runInstruction (Memory xs (y:ys)) Rgt = return (return (Memory (y:xs) ys), return ())
-runInstruction (Memory (x:xs) ys) Lft = return (return (Memory xs (x:ys)), return ())
-runInstruction (Memory [] ys) Lft = return (return (Memory [] (0:ys)), return ())
-runInstruction (Memory xs (y:ys)) Inc = return (return (Memory xs ((y+1):ys)), return ())
-runInstruction (Memory xs (y:ys)) Dec = return (return (Memory xs ((y-1):ys)), return ())
-runInstruction mem@(Memory _ (x:xs)) Out = return (return mem, putChar (chr x))
-runInstruction (Memory xs (y:ys)) Inp = return (mem', return ())
-                                          where mem' = getChar >>= \z -> return (Memory xs ((ord z):ys))
-runInstruction mem@(Memory xs (0:ys)) (Loop _) = return (return mem, return ())
-runInstruction mem (Loop prog) = do
-                                    (mem1, act') <- runProgram mem prog
-                                    mem' <- mem1
-                                    (mem'', act'') <- runInstruction mem' (Loop prog)
-                                    return (mem'', act' >> act'')
+runInstruction :: Instruction -> Memory -> IO Memory
+runInstruction Nop mem  = pure mem
+-- moving the location of the head
+runInstruction Rgt (Memory xs (y:[])) = pure (Memory (y:xs) [0])
+runInstruction Rgt (Memory xs (y:ys)) = pure (Memory (y:xs) ys)
+runInstruction Lft (Memory (x:xs) ys) = pure (Memory xs (x:ys))
+runInstruction Lft (Memory [] ys)     = pure (Memory [] (0:ys))
+-- loop until the head reads 0
+runInstruction (Loop _) mem@(Memory xs (0:ys))  = pure mem
+runInstruction (Loop p) mem                     = runProgram p mem 
+                                                  >>= runInstruction (Loop p)
+-- increment/decrement the head
+runInstruction Inc (Memory xs (y:ys)) = pure (Memory xs ((y+1):ys))
+runInstruction Dec (Memory xs (y:ys)) = pure (Memory xs ((y-1):ys))
+-- output the head
+runInstruction Out mem@(Memory _ (x:xs)) = putChar (chr x) >> pure mem
+-- get input and store it in the head
+runInstruction Inp(Memory xs (y:ys))     = getChar >>= \z -> pure (Memory xs ((ord z):ys))
 
-
-
-runProgram :: Memory -> Program -> IO (IO Memory, IO ())
-runProgram mem [] = return (return mem, return ())
-runProgram mem (i:is) = do
-                           (mem1, act1) <- runInstruction mem i
-                           mem' <- mem1
-                           (mem2, act2) <- runProgram mem' is
-                           return (mem2, act1 >> act2)
+runProgram :: Program -> Memory -> IO Memory
+runProgram is = foldr (>=>) pure (runInstruction <$> is) 
 
 main :: IO ()
 main = do
-   hSetBuffering stdin NoBuffering 
    (fileName:_) <- getArgs
-   prog <- readFile fileName
-   (mem, act) <- runProgram emptyMem (parse (filter (`elem` "[]+-><.,") prog))
-   act
+   prog <- (parse . (filter (`elem` "[]+-><.,"))) <$> readFile fileName
+   void $ runProgram prog emptyMem
